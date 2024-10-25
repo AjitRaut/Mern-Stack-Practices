e6030774d9e08939bd1c52a870d1aa5000ffb826
Ajit raut <ajitraut9561@gmail.com>
2024-10-25 21:52:12 +0530

    delete added

--- To-Do List/server/controllers/todoController.js
+++ To-Do List/server/controllers/todoController.js
@@ -8,11 +8,40 @@ const createTodo = async (req, res) => {
       return res.status(500).json({ msg: "Fill the task" });
     }
     const new_todo = new Todo({ task });
-    await new_todo.save;
-    res.status(200).json({ msg: new_todo });
+    await new_todo.save();
+    res.status(200).json({ new_todo });
   } catch (error) {
     res.status(500).json({ msg: "Server Error" });
   }
 };
 
-module.exports = { createTodo };
+const getTodos = async (req, res) => {
+  try {
+    const Todos = await Todo.find();
+    if (!Todos) {
+      return res.status(404).json({ msg: "Todods Not Found" });
+    }
+    res.status(200).json({ data: Todos });
+    console.log(Todos);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteTodo = await Todo.findById(id);
+
+    if (!deleteTodo) {
+      return res.status(404).json({ ms: "Todo not found" });
+    }
+
+    await Todo.findByIdAndDelete(id);
+    res.status(200).json({ msg: deleteTodo });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createTodo, getTodos , deleteTodo };
